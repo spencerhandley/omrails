1149e8414caac7eea93d48da39be9422ff102837
spencer handley <spencerhandley@gmail.com>
2013-08-17 07:13:18 -0600

    Add User Model With Devise

--- Gemfile
+++ Gemfile
@@ -1,7 +1,11 @@
 source 'https://rubygems.org'
 
-gem 'rails', '3.2.12'
+gem 'rails', '4.0.0'
 gem 'jquery-rails'
+gem 'devise'
+gem 'simple_form'
+gem "activemodel", "~> 4.0.0"
+gem 'protected_attributes'
 
 group :production do 
 	gem 'pg'
@@ -11,10 +15,9 @@ group :development, :test do
 	gem 'sqlite3'
 end
 
-group :assets do
-  gem 'sass-rails',   '~> 3.2.3'
-  gem 'coffee-rails', '~> 3.2.1'
+  gem 'sass-rails',   '~> 4.0.0'
+  gem 'coffee-rails', '~> 4.0.0'
 
   gem 'uglifier', '>= 1.0.3'
 gem 'bootstrap-sass', '~> 2.3.2.0'
-end
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ Omrails::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  
+
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
@@ -64,4 +66,5 @@ Omrails::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
